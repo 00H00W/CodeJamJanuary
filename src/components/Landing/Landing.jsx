f039ea91797403c6ef9496985ec150c078fafc67
Sam Branham <hotdogsb@gmail.com>
2025-01-29 08:01:07 -0600

    Update landing page - Add searchbar component

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -2,10 +2,10 @@ import "./Landing.css";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { search } from "../../Utils/ShelterAPI";
+import SearchBar from "../SearchBar/SearchBar";
 
 function Landing({ searchLocation, handleSearchChange }) {
   const navigate = useNavigate();
-
   const handleSubmit = (e) => {
     e.preventDefault();
     search();
@@ -17,33 +17,16 @@ function Landing({ searchLocation, handleSearchChange }) {
       <div className="hero__header">
         <div className="hero__overlay"></div>
         <h1 className="hero__title">Sh3ltr</h1>
-        <p id="nav-search-target" className="search__subtitle">
+        <p className="hero__subtitle">
           Find a place to stay in times of disaster
         </p>
-        <form onSubmit={handleSubmit} action="" className="search__form">
-          <label htmlFor="search-input" className="form__label">
-            Your Location:
-            <input
-              onChange={handleSearchChange}
-              value={searchLocation}
-              className="search__input"
-              required
-              type="text"
-              placeholder="los angeles"
-              id="search-input"
-            />
-          </label>
-          <label htmlFor="search-input" className="form__btn-label">
-            <button
-              type="submit"
-              htmlFor="search-input"
-              className="search__btn"
-              //   onClick={handleSubmit}
-            >
-              Search
-            </button>
-          </label>
-        </form>
+        <div className="hero__search-bar">
+          <SearchBar
+            handleSubmit={handleSubmit}
+            handleSearchChange={handleSearchChange}
+            searchLocation={searchLocation}
+          />
+        </div>
       </div>
     </section>
   );
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,34 @@
+import "./SearchBar.css";
+
+function SearchBar({ searchLocation, handleSearchChange, handleSubmit }) {
+  return (
+    <div className="search-bar">
+      <form onSubmit={handleSubmit} action="" className="search-bar__form">
+        <label htmlFor="search-input" className="search-bar__form-label">
+          Your Location:
+        </label>
+        <span className="search-bar__border">
+          <input
+            onChange={handleSearchChange}
+            value={searchLocation}
+            className="search-bar__form-input"
+            required
+            type="text"
+            placeholder="los angeles"
+            id="search-input"
+          />
+          <button
+            type="submit"
+            htmlFor="search-input"
+            className="search-bar__form-submit"
+            //   onClick={handleSubmit}
+          >
+            Search
+          </button>
+        </span>
+      </form>
+    </div>
+  );
+}
+
+export default SearchBar;
