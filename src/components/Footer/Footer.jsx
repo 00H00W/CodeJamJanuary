4180859715684a78601c697e12f084c2be49461d
Jake B <jakeburke1775@gmail.com>
2025-01-28 19:20:26 -0500

    style update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,118 +3,118 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer id="nav-contact-target" className="footer">
-      <ul className="footer__person-container">
-        <li className="footer__person">
-          <h4 className="footer__person-title">Jacob Burke</h4>
-          <div className="footer__person-link-container">
+      <ul className="people">
+        <li className="people__person">
+          <h4 className="people__person-name">Jacob Burke</h4>
+          <div className="people__person-link-container">
             <a
               href="https://github.com/jakeburke1775"
-              className="footer__person-link text-link"
+              className="people__person-link text-link"
             >
               <img
                 src="/src/assets/3.jpeg"
                 alt="GitHubImg"
-                className="footer__person-link-img"
+                className="people__person-link-img"
               />
               GitHub
             </a>
           </div>
-          <div className="footer__person-link-container">
+          <div className="people__person-link-container">
             <a
               href="https://www.linkedin.com/in/jake-burke-468b13143/"
-              className="footer__person-link text-link"
+              className="people__person-link text-link"
             >
               <img
                 src="/src/assets/3.jpeg"
                 alt="LinkedInImg"
-                className="footer__person-link-img"
+                className="people__person-link-img"
               />
               LinkedIn
             </a>
           </div>
         </li>
-        <li className="footer__person">
-          <h4 className="footer__person-title">Sam Branham</h4>
-          <div className="footer__person-link-container">
+        <li className="people__person">
+          <h4 className="people__person-name">Sam Branham</h4>
+          <div className="people__person-link-container">
             <a
               href="https://github.com/00H00W"
-              className="footer__person-link text-link"
+              className="people__person-link text-link"
             >
               <img
                 src="/src/assets/3.jpeg"
                 alt="GitHubImg"
-                className="footer__person-link-img"
+                className="people__person-link-img"
               />
               GitHub
             </a>
           </div>
-          <div className="footer__person-link-container">
+          <div className="people__person-link-container">
             <a
               href="https://www.linkedin.com/in/sambranham/"
-              className="footer__person-link text-link"
+              className="people__person-link text-link"
             >
               <img
                 src="/src/assets/3.jpeg"
                 alt="LinkedInImg"
-                className="footer__person-link-img"
+                className="people__person-link-img"
               />
               LinkedIn
             </a>
           </div>
         </li>
-        <li className="footer__person">
-          <h4 className="footer__person-title">Juan Alcantara</h4>
-          <div className="footer__person-link-container">
+        <li className="people__person">
+          <h4 className="people__person-name">Juan Alcantara</h4>
+          <div className="people__person-link-container">
             <a
               href="https://github.com/jcalcan"
-              className="footer__person-link text-link"
+              className="people__person-link text-link"
             >
               <img
                 src="/src/assets/3.jpeg"
                 alt="GitHubImg"
-                className="footer__person-link-img"
+                className="people__person-link-img"
               />
               GitHub
             </a>
           </div>
-          <div className="footer__person-link-container">
+          <div className="people__person-link-container">
             <a
               href="https://www.linkedin.com/in/juan-alcantara-9aa502301/"
-              className="footer__person-link text-link"
+              className="people__person-link text-link"
             >
               <img
                 src="/src/assets/3.jpeg"
                 alt="LinkedInImg"
-                className="footer__person-link-img"
+                className="people__person-link-img"
               />
               LinkedIn
             </a>
           </div>
         </li>
-        <li className="footer__person">
-          <h4 className="footer__person-title">Gerard Ike</h4>
-          <div className="footer__person-link-container">
+        <li className="people__person">
+          <h4 className="people__person-name">Gerard Ike</h4>
+          <div className="people__person-link-container">
             <a
               href="https://github.com/gerard415"
-              className="footer__person-link text-link"
+              className="people__person-link text-link"
             >
               <img
                 src="/src/assets/3.jpeg"
                 alt="GitHubImg"
-                className="footer__person-link-img"
+                className="people__person-link-img"
               />
               GitHub
             </a>
           </div>
-          <div className="footer__person-link-container">
+          <div className="people__person-link-container">
             <a
               href="https://www.linkedin.com/in/chinedum-ike/"
-              className="footer__person-link text-link"
+              className="people__person-link text-link"
             >
               <img
                 src="/src/assets/3.jpeg"
                 alt="LinkedInImg"
-                className="footer__person-link-img"
+                className="people__person-link-img"
               />
               LinkedIn
             </a>
