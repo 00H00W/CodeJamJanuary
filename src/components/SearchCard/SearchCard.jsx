fa487d857e74929b95a00c087bf53696ef0b451b
Sam Branham <hotdogsb@gmail.com>
2025-01-29 16:43:36 -0600

    Update card style

--- src/components/SearchCard/SearchCard.jsx
+++ src/components/SearchCard/SearchCard.jsx
@@ -1,18 +1,19 @@
 import "./SearchCard.css";
 
-function SearchCard({place}) {
+function SearchCard({ place }) {
   return (
-    <li className="search-card">
-      <a className="search__card-link" href={place.websiteUri} target="_blank" rel="noopener noreferrer">
-        <h3 className="search-card__title">{place.displayName.text}</h3>
-        <p className="search-card__label">Address:</p>
-        <p className="search-card__value">{place.formattedAddress}</p>
-        <p className="search-card__label">Phone:</p>
-        <p className="search-card__value">{place.nationalPhoneNumber}</p>
-      </a>
+    <li
+      className="search-card"
+      onClick={() => {
+        window.open(place.websiteUri, "_blank", "noopener,noreferrer");
+      }}
+    >
+      <h3 className="search-card__title">{place.displayName.text}</h3>
+      <p className="search-card__label">Address:</p>
+      <p className="search-card__value">{place.formattedAddress}</p>
+      <p className="search-card__label">Phone:</p>
+      <p className="search-card__value">{place.nationalPhoneNumber}</p>
     </li>
-
-    
   );
 }
 
